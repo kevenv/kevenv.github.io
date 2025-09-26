6ce37f7f6a3610442ec282559c420ed8571d9533
Keven Villeneuve <keven.villeneuve@gmail.com>
2025-09-25 20:11:36 -0400

    notes: move fedora setup to obsidian

--- tools/build_site.py
+++ tools/build_site.py
@@ -71,8 +71,6 @@ if __name__ == '__main__':
             ('notes/sort_algos','../','Sorting algorithms'),
             ('notes/priority_queue','../','Priority queue'),
             ('notes/css_cheatsheet','../','CSS cheatsheet'),
-            ('notes/fedora_setup','../','Fedora setup'),
-            ('notes/fedora_nvidia','../','Fedora + Nvidia'),
 
             # ('notes/nnet_backprop','../','Backprop'),
             ('notes/nnet','../','Neural networks'),
